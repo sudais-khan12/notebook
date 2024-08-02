fe2bef6c6453d9c4d391891723e9785e36b50f16
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2024-08-02 12:32:31 +0500

    updated

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const connection = require("./db");
+const express = require("express");
+
+connection();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes.js"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port);
---
+++ server/models/Notes.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const NoteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    default: "general",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Note", NoteSchema);
