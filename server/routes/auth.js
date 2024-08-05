eb5debb28df1c894ad024ad9094488e0b1f6e1c4
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2024-08-05 12:52:44 +0500

    modified

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a valid name").isLength({ min: 3, max: 15 }),
     body(
@@ -17,17 +17,28 @@ router.post(
       ),
     body("email", "Enter a valid email").isEmail(),
   ],
-  (req, res) => {
+
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.send("done");
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
