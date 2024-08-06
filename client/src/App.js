2cfb7dd62eb8f333aa3a2a05e95f4bb62fae63af
sudais-khan12 <155130846+sudais-khan12@users.noreply.github.com>
2024-08-06 22:18:38 +0500

    changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <>
-  <Navbar/>
-  </>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" elFement={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
